97667740b3594e06bf0ca2b4e0081c6af97a356e
{cocoide} <{cocoide.com@gmail.com}>
2023-01-28 20:43:25 +0900

    Feat auto focus with useRef at header search button

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,14 @@
 "use client"
 import Link from 'next/link'
-import { MagnifyingGlassIcon, Squares2X2Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import { useState } from 'react'
+import { MagnifyingGlassIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { useRef, useState } from 'react'
 
 const Header = () => {
+    const inputRef = useRef<HTMLInputElement>(null);
     const [isMenuOpen, setMenuOpen] = useState(false)
+    const [query, setQuery] = useState("")
     return (
-        <div className="">
+        <div className="sticky top-0 z-40">
         <div className="flex justify-between items-center py-2 px-5  md:px-10
          bg-nothing h-18 border-b border-shadow">
 
@@ -17,10 +19,14 @@ const Header = () => {
                 <button className="text-white bg-primary font-bold p-2 rounded-md  text-base font-base"
                 >サインイン</button>
                     <button
-                        onClick={() => setMenuOpen(!isMenuOpen)}
+                        onClick={async () => {
+                            await setMenuOpen(!isMenuOpen)
+                            return (!isMenuOpen) && inputRef && inputRef.current?.focus();
+
+                        }}
                         className="">
                         {(!isMenuOpen) ?
-                            <Squares2X2Icon className="h-10 w-10 text-primary" />
+                            <MagnifyingGlassIcon className="h-10 w-10 text-primary" />
                             :
                             <XMarkIcon className="h-8 w-8 mx-1 text-primary" />
                         }
@@ -29,19 +35,29 @@ const Header = () => {
             </div>
 
             {isMenuOpen &&
-                <div className="flex flex-col  border-b border-shadow py-2 px-5 md:py-5  md:px-[15%] bg-nothing divide-y divide-shadow ">
+                <div className="absolute inset-x-0 flex flex-col  border-b border-shadow py-2 px-5 md:py-5  md:px-[15%] bg-nothing divide-y divide-shadow z-30
+                shadow-md">
                     <div className="flex items-center w-[100%] px-2 bg-shadow rounded-md mb-2 md:mb-5">
-                        <MagnifyingGlassIcon className="h-5 w-5 text-gray-500" />
-                        <input type="search" placeholder="キーワード、#ハッシュタグ." className="bg-shadow w-[100%] p-1 border-transparent focus:ring-0 border-none" />
+                        <MagnifyingGlassIcon className="h-5 w-5  text-gray-500" />
+                        <input ref={inputRef}
+                            type="search" placeholder="キーワード、#ハッシュタグ" className="bg-shadow w-[100%] p-1 border-transparent focus:ring-0 border-none h-10" />
                     </div>
-
+                    {
+                        (query.length > 0) ?
+                            <div className="">
+                                <div>教育</div>
+                                <div>プログラミング</div>
+                            </div>
+                            :
+                            <></>
+                    }
                     <Link href={"/"}><h2 className="p-2 rounded-md hover:bg-neutral duration-100">✍🏻質問・議題を作成</h2></Link>
                     <Link href={"/"}><h2 className="p-2 rounded-md hover:bg-neutral duration-100">💭回答・議論する</h2></Link>
                     <Link href={"/seminar"}><h2 className="p-2 rounded-md hover:bg-neutral duration-100">📚ゼミナール</h2></Link>
                     <Link href={"/"}><h2 className="p-2 rounded-md hover:bg-neutral duration-100">🗣Quasta(クアスタ)に関するFAQ</h2></Link>
-                    <Link href={"/"}><h2 className="p-2 rounded-md hover:bg-neutral duration-100">💻設定</h2></Link>
+                    <Link href={"/"}><h2 className="p-2 rounded-md hover:bg-neutral duration-100 border-b border-shadow">💻設定</h2></Link>
                 </div>
-            }
+            } 
         </div>
     )
 }
