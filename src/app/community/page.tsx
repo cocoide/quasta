7f79a6a83f0dfe5ba4d42b93355a85bf6f6f976a
{cocoide} <{cocoide.com@gmail.com}>
2023-03-13 17:46:24 +0900

    Fix deploy error

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -3,22 +3,19 @@ import LaunchCommunity from './LaunchCommunity';
 import { API_URL } from '../../libs/consts';
 import CommunityLists from './CommunityLists';
 
-async function getCommunity() {
-    const res = await fetch(`${API_URL}/community`)
-    if (!res.ok) {
-        throw new Error('Failed to fetch data');
-    }
-    return res.json();
-}
+// async function getCommunity() {
+//     const res = await fetch(`${API_URL}/community`)
+//     return res.json();
+// }
 
 const page = async () => {
 
-    const community = await getCommunity()
+    // const community = await getCommunity()
 
     return (
         <div className="">
             <LaunchCommunity />
-            <CommunityLists community={community} />
+            {/* <CommunityLists community={community} /> */}
         </div>
     )
 }
